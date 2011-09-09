24f23ac9f8002cfcd25d3277809b024e5b7df992
s070506 <s070506@s070506>
2011-09-09 15:32:06 +0200

    First commit, no actual code has been added

---
+++ src/clusterfinder/Main.java
@@ -0,0 +1,20 @@
+package clusterfinder;
+
+/** ClusterFinder main module
+ * This application takes a series of points on a 2-dimential grid and
+ * canculates a given number of clusters.
+ * 
+ * @author Nicky Advokaat, Carl van Dueren den Hollander, Tim van Dalen, Roby Visser, Ferry Timmers
+ * @since 8-9-2011
+ * @version 1.0.0.1
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+}
